ce9df703a998ada0e88ae2c1e869837ec0ea1e10
Gohar <goharpapinyan@gmail.com>
2020-02-03 12:43:38 -0800

    Forgot Password tests added

--- src/main/java/pages/ForgotPasswordPage.java
+++ src/main/java/pages/ForgotPasswordPage.java
@@ -5,8 +5,11 @@ import org.openqa.selenium.WebDriver;
 
 public class ForgotPasswordPage extends BasePage{
 
+    private final By FORGOT_PASSWORD_LABEL = By.cssSelector("div.example h2");
     private final By EMAIL_FIELD = By.id("email");
     private final By RETRIEVE_PASSWORD = By.id("form_submit");
+    private final By EMAIL_SENT_TEXT = By.id("content");
+    private final By INTERNAL_SERVER_ERROR_TEXT = By.tagName("h1");
 
     /**
      * Constructor
@@ -25,4 +28,13 @@ public class ForgotPasswordPage extends BasePage{
         click(RETRIEVE_PASSWORD);
     }
 
+    public String getHeaderText() { return getText(FORGOT_PASSWORD_LABEL);}
+
+    public String getEmailSentText() {
+        return getText(EMAIL_SENT_TEXT);
+    }
+
+    public String getErrorText(){
+        return getText(INTERNAL_SERVER_ERROR_TEXT);
+    }
 }
